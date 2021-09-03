1acc05d1220bf43dd4c3754af60f8dbcb16a7849
IliusSagar <sagar01830@gmail.com>
2021-09-02 20:44:54 -0700

    login, dashboard developed

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//suppliers routes are here--------------------
